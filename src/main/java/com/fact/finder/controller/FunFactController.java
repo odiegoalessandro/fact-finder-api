c272a12a7c7b74004c52f9a11f8ed2ffd2173584
Diego alessandro da cruz martins <diegoalessandrodacruzmartins@gmail.com>
2023-12-29 00:25:03 -0300

    feat: create initial controller

--- src/main/java/com/fact/finder/controller/FunFactController.java
+++ src/main/java/com/fact/finder/controller/FunFactController.java
@@ -1,13 +1,36 @@
 package com.fact.finder.controller;
 
+import com.fact.finder.model.FunFact;
 import com.fact.finder.repository.FunFactRespository;
+import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Random;
 
 @RestController
+@RequestMapping("/facts")
 public class FunFactController {
     @Autowired
     FunFactRespository factRespository;
 
+    @GetMapping("/all")
+    public ResponseEntity<List<FunFact>> getRandomFacts(){
+        var facts = factRespository.findAll();
+
+        return ResponseEntity.status(200).body(facts);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createFact(@RequestBody FunFact funFact){
+        FunFact fact = new FunFact();
+        fact.setBody(funFact.getBody());
+        fact.setTitle(funFact.getTitle());
+        fact.setSource(funFact.getSource());
+
+        return ResponseEntity.status(200).body(factRespository.save(fact).toString());
+    }
 
 }
